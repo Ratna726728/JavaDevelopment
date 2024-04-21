b98159c54b372c55abbf4545ffeb7363eb476e16
Ratnadeep <ratna726728@gmail.com>
2024-04-21 15:49:32 +0530

    adding day 31 notes on string

---
+++ NOTES/Codes/Day31/src/com/creation/StringDemo.java
@@ -0,0 +1,12 @@
+package com.creation;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		
+		String s = "Ajit";
+		s = s.concat("Rajesh");
+		
+		System.out.println(s);
+	}
+}
---
+++ NOTES/Codes/Day31/src/com/creation/StringDemo1.java
@@ -0,0 +1,11 @@
+package com.creation;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = new String("java");
+		System.out.println(s1 = s2);
+		System.out.println(s1.equals(s2));
+	}
+}
