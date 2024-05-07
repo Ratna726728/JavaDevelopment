c57b97c753a2bf30029c986154874a0d9bf4b9af
Ratnadeep <ratna726728@gmail.com>
2024-05-07 22:39:06 +0530

    adding day 45 codes

---
+++ NOTES/Codes/Day45/src/com/tree/TreeSetDemo.java
@@ -0,0 +1,15 @@
+package com.tree;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		TreeSet treeset = new TreeSet();
+		treeset.add("jay");
+		treeset.add("mahesh");
+		treeset.add("sam");
+		System.out.println(treeset);
+		
+	}
+}
