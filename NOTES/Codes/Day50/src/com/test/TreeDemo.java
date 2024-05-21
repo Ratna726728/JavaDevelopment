56155af4d49337cf3f9f688781cdecc566dcc49a
Ratnadeep <ratna726728@gmail.com>
2024-05-21 16:28:40 +0530

    adding day50 codes

---
+++ NOTES/Codes/Day50/src/com/test/TreeDemo.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.util.TreeMap;
+
+public class TreeDemo {
+	
+	public static void main(String[] args) {
+		
+		TreeMap treemap = new TreeMap();
+		treemap.put(10, "Sukuna");
+		treemap.put(20, "yuji");
+		treemap.put(15, "gojo");
+		
+		System.out.println(treemap);
+	}
+
+}
