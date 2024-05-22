f343a07e0c06dcc7828397f5522a6ad61fcf8d43
Ratnadeep <ratna726728@gmail.com>
2024-05-22 16:26:21 +0530

    adding day 51 codes

---
+++ NOTES/Codes/Day51/src/com/test/HashTableDemo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.Hashtable;
+
+public class HashTableDemo {
+
+	public static void main(String[] args) {
+		Hashtable hashTable = new Hashtable();
+		hashTable.put(10, "shri");
+		hashTable.put(11, "manish");
+		hashTable.put(12, "mahesh");
+		System.out.println(hashTable);
+	}
+}
